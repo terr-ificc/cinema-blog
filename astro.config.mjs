f1e3275a42af27a94d088db73387fd96f629bc7a
unknown <runaway.kit09@gmail.com>
2026-09-01 19:57:25 +0700

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://terr-iffic.github.io',
+  base: '/cinema-blog',
+});
